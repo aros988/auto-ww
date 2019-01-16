6cfc631e80845ede88c913e339dd73f01017cfc7
Arkadiusz Krezel <arkadiuszkrezel88@gmail.com>
2019-01-16 13:02:46 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './scss/style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+
+ReactDOM.render(<App />, document.getElementById('app'));
