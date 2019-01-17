b192c17e94ef8e8f548d5189d7d2350ab01b386e
Arkadiusz Krezel <arkadiuszkrezel88@gmail.com>
2019-01-17 13:24:26 +0100

    Added Select Form to site

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import SelectForm from './SelectForm';
 
 class App extends Component {
-    render () {
-        return (
-            <div><h1>Witam</h1></div>
-        );
-    }
-};
+    render() {
+        return <div>
+          <h1>Rent a Car with ReactJS</h1>
+          <div style={{margin: '16px', position: 'relative'}}>
+            <SelectForm
+              name="rent[auto_id]"
+              items={[
+                { value: 'BMW X3', id: 1 },
+                { value: 'Ford Mustang GT', id: 2 },
+                { value: 'Skoda Fabia', id: 3 },
+                { value: 'Renault Megane', id: 4 },
+              ]}
+            />
+          </div>
+        </div>
+      }
+    };
 
 export default App;
