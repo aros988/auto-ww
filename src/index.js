7b14835ad7e47452f08c4647b94fba9624378853
Arkadiusz Krezel <arkadiuszkrezel88@gmail.com>
2019-03-06 17:23:22 +0100

    navbar added and router

--- src/index.js
+++ src/index.js
@@ -2,4 +2,29 @@ import './scss/style.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
-ReactDOM.render(<App />, document.getElementById('app'));
+import CarList from './components/CarList';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+
+const routing = (
+    <Router>
+        <div>
+            <div className="navbar">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/carlist">Car Details</Link>
+                    </li>
+                </ul>
+            </div>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/carlist" component={CarList} />
+            </Switch>
+
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('app'));
